63fafe5c42e74e4db6df992591de0b1e55667b52
fariz1705 <farizthn@gmail.com>
2023-06-12 15:39:10 +0700

    tugas kelompok

---
+++ application/modules/customer/views/footer.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+  
+
+<script src="<?php echo get_theme_uri('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js', 'adminlte'); ?>"></script>
+<script src="<?php echo get_theme_uri('js/adminlte.js', 'adminlte'); ?>"></script>
+
+<!-- Elapsed in {elapsed_time} times  -->
+</body>
+</html>
